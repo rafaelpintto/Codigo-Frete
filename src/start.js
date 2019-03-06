7951a2e582a682dad4aedb56b611ae91fcfddb00
Luiz Felipe das Neves Lopes <luizfelipe2310@gmail.com>
2019-03-05 23:48:43 -0300

    Update start.js

--- src/start.js
+++ src/start.js
@@ -4,4 +4,4 @@ require('./modulos/main'); // check fretebras
 require('./modulos/index'); // index
 
 // don`t sleep heroku, get every 5 minutes (300000)
-setInterval(() => { http.get('http://fretebras-truckpad.herokuapp.com'); }, 300000);
+setInterval(() => { http.get('http://clawler-fretebras.herokuapp.com'); }, 300000);
