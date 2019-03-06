d7f8e5d6ec6e6d637b9b66524ad2b08fc6e78180
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2019-03-06 03:07:06 -0300

    Update main.js

--- src/modulos/main.js
+++ src/modulos/main.js
@@ -9,28 +9,31 @@ const bd = require('../modulos/bd'); // database
     await browser.close();
 })();
 
-async function listLinks(url, page) {   
+async function listLinks(url, page) {
     await console.log('l', url);
     await page.goto(url);
-
-    const base = 'https://www.fretebras.com.br';   
-    let next = 'https://www.fretebras.com.br/fretes';
-
-    if (await page.$('.lista-pg-prox.dis') === null) {
-        const filtro = await page.evaluate(base => Array.from( document.querySelectorAll('div#corpo-lista-pg > a'), (element => base + element.getAttribute('href'))), base);
-        next = await filtro.reverse()[0];
-    }
+    if(checkExist)
+    {
+        const base = 'https://www.fretebras.com.br';   
+        let next = 'https://www.fretebras.com.br/fretes';
     
-    const links = await page.evaluate(base => Array.from( document.querySelectorAll( '.col5-quadro-imagem a' ), (element => base + '/' + element.getAttribute('href'))), base);
-    for(let index = 0; index < links.length; index++) if(await !!bd.Checkdb(links[index])) await getinfo(links[index], page);
-
-    await listLinks(next, page);
+        if (await page.$('.lista-pg-prox.dis') === null) {
+            const filtro = await page.evaluate(base => Array.from( document.querySelectorAll('div#corpo-lista-pg > a'), (element => base + element.getAttribute('href'))), base);
+            next = await filtro.reverse()[0];
+        }
+        
+        const links = await page.evaluate(base => Array.from( document.querySelectorAll( '.col5-quadro-imagem a' ), (element => base + '/' + element.getAttribute('href'))), base);
+        for(let index = 0; index < links.length; index++) if(await !!bd.Checkdb(links[index])) await getinfo(links[index], page);
+    
+        await listLinks(next, page);
+    }
+    else await console.log('Banimento de IP');
 }
 
-/*
 async function checkExist(page){
-    return (await page.$('.origem > span') !== null);
+    return (await page.$('td > span.style4 > strong') === null);
 }
+/*
 async function checkIP(page) {
     let status = await page.$('.dialog p') === null
     if(!status){ 
@@ -43,37 +46,40 @@ async function checkIP(page) {
 
 async function getinfo(url, page) {
     try {
-        await console.log('g', url)
+        await console.log('g', url);
         await page.goto(url);
-        // image, empresa, produto
-        const info = await page.evaluate((url) => {
-            const ocidade = document.querySelector('div.origem a.cor-vermelho:nth-of-type(1)').textContent
-            const oestado = document.querySelector('div.origem a.cor-vermelho:nth-of-type(2)').textContent
-            const origem = ocidade + '/' + oestado
-            const dcidade = document.querySelector('div.destino a.cor-vermelho:nth-of-type(1)').textContent
-            const destado = document.querySelector('div.destino a.cor-vermelho:nth-of-type(1)').textContent
-            const destino = dcidade + '/' + destado
-            const km = document.querySelector('.frete-dados.frete-km').textContent
-            let preco = document.querySelector('.frete-dados.frete-preco').textContent
-            const peso = 'N/A'
-            const veiculo = document.querySelector('.frete-dados.frete-veiculos').textContent // loop array
-            
-            const info = {
-                url,
-                origem,
-                destino,
-                km,
-                preco,
-                peso,
-                veiculo,
-                site: 'fretebras'
-            }
-            return info;
-        }, url);
-        await bd.Insertdb(info);
+        if(checkExist){
+            // image, empresa, produto
+            const info = await page.evaluate((url) => {
+                const ocidade = document.querySelector('div.origem a.cor-vermelho:nth-of-type(1)').textContent
+                const oestado = document.querySelector('div.origem a.cor-vermelho:nth-of-type(2)').textContent
+                const origem = ocidade + '/' + oestado
+                const dcidade = document.querySelector('div.destino a.cor-vermelho:nth-of-type(1)').textContent
+                const destado = document.querySelector('div.destino a.cor-vermelho:nth-of-type(1)').textContent
+                const destino = dcidade + '/' + destado
+                const km = document.querySelector('.frete-dados.frete-km').textContent
+                let preco = document.querySelector('.frete-dados.frete-preco').textContent
+                const peso = 'N/A';
+                const veiculo = document.querySelector('.frete-dados.frete-veiculos').textContent // loop array
+                
+                const info = {
+                    url,
+                    origem,
+                    destino,
+                    km,
+                    preco,
+                    peso,
+                    veiculo,
+                    site: 'fretebras'
+                }
+                return info;
+            }, url);
+            await bd.Insertdb(info);
+        }
+        else await console.log('Banimento de IP');
     } 
     catch (error) { 
-        await console.log('deu ruim',url, error) 
+        await console.log('deu ruim',url, error); 
     }
-    await sleep.sleep(1);
+    await sleep.sleep(2);
 }
